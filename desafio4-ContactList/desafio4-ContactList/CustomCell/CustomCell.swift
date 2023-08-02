7e9dc603da7cf209ed1d7ce471666ef08d9a0713
sergioHEFE <121230365+SergioHenriqueFe@users.noreply.github.com>
2023-08-02 08:32:32 -0300

    gallery abrindo e salvando na tableView

--- desafio4-ContactList/desafio4-ContactList/CustomCell/CustomCell.swift
+++ desafio4-ContactList/desafio4-ContactList/CustomCell/CustomCell.swift
@@ -19,12 +19,36 @@ class CustomCell: UITableViewCell {
     static func nib()->UINib {
         return UINib(nibName: self.identifier, bundle: nil)
     }
+    var recebeNome:String?{
+        didSet{
+            recebidoNameLabel.text = recebeNome
+        }
+    }
+    var recebeNumber:String?{
+        didSet{
+            recebidoNumberLabel.text = recebeNumber
+        }
+    }
+    var recebeImage:UIImage?{
+        didSet{
+            recebidoPerfilImageVIew.image = recebeImage
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
+    func configImageVIew(){
+        recebidoPerfilImageVIew.layer.cornerRadius = recebidoPerfilImageVIew.frame.size.width/2
+        recebidoPerfilImageVIew.clipsToBounds = true
+        recebidoPerfilImageVIew.layer.masksToBounds = true
+    }
+    
 
     
 }
+
+
+
--- desafio4-ContactList/desafio4-ContactList/Tela01.swift
+++ desafio4-ContactList/desafio4-ContactList/Tela01.swift
@@ -22,8 +22,31 @@ class Tela01: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configTableView()
+        configImageView()
+        configButton()
+        configTexFiel()
     }
+//MARK: Configurações de item da tela
+    func configImageView(){
+        //Deixar a imagem redonda
+        perfilImageView.layer.cornerRadius = perfilImageView.frame.size
+            .width/2.0
+        perfilImageView.clipsToBounds = true
+        perfilImageView.layer.masksToBounds = true
+        perfilImageView.contentMode = .scaleToFill
+        
+    }
+    
+    func configButton(){
+        addPhotoButton.setTitle("Adicionar Foto", for: .normal)
+        saveContactButton.setTitle("Salvar contato", for: .normal)
+    }
+    
     func configTexFiel(){
+        nameTextField.delegate = self
+        NumberTextField.delegate = self
+        nameTextField.placeholder  = "Digite o nome"
+        NumberTextField.placeholder = "Digite o telefone"
         
     }
     func configTableView(){
@@ -32,13 +55,25 @@ class Tela01: UIViewController {
         tableView.register(CustomCell.nib(), forCellReuseIdentifier: CustomCell.identifier)
         tableView.backgroundColor = UIColor.blue
     }
-
-    @IBAction func tappedAddPhoto(_ sender: Any) {
-        
-    }
+//MARK: IBaction
     
+    @IBAction func tappedAddPhoto(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+//    var customCell:CustomCell?
+    var data: [(name: String,number: String, image :UIImage)] = []
     @IBAction func tappedSaveContactButton(_ sender: Any) {
+        guard let name = nameTextField.text, let number = NumberTextField.text, let image = perfilImageView.image else {return}
+        data.append((name,number,image))
+        tableView.reloadData()
         
+        nameTextField.text = ""
+        NumberTextField.text = ""
+        perfilImageView.image = .init(systemName: "person")
+        view.endEditing(true)
     }
     
     
@@ -49,13 +84,20 @@ class Tela01: UIViewController {
 extension Tela01 : UITableViewDelegate,UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath) as? CustomCell
+        let item = data[indexPath.row]
+        var configNome:String = "Nome: \(item.name)"
+        var configNumber:String = "Telefone:  \(item.number)"
+        cell?.recebeNome = configNome
+        cell?.recebeNumber = configNumber
+        cell?.recebeImage = item.image
+        return cell ?? UITableViewCell()
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         100
     }
@@ -63,4 +105,25 @@ extension Tela01 : UITableViewDelegate,UITableViewDataSource{
 
 //MARK: TexField Delegate
 
+extension Tela01 :UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+}
 
+//MARK: UIImagePicker
+extension Tela01 : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            perfilImageView.image = pickedImage // Define a imagem selecionada na UIImageView.
+        }
+        
+        picker.dismiss(animated: true, completion: nil) // Fecha a galeria após a seleção da imagem.
+    }
+    // Função que é chamada quando o usuário cancela a seleção de imagem na galeria.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil) // Fecha a galeria após o cancelamento.
+    }
+    
+}
